eeb52908d92163e728d8600cf7c41dffd4792c10
TakuyaOSADA <osada@TakuyaOSADA-no-Mac-mini.local>
2014-05-06 10:14:51 +0900

    modify yum-update cookbook

--- site-cookbooks/yumsetup/recipes/default.rb
+++ site-cookbooks/yumsetup/recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+yum_package "yum-fastestmirror" do
+  action :install
+end
+
+execute "yum-update" do
+  user "root"
+  command "yum -y update"
+  action :run
+end
