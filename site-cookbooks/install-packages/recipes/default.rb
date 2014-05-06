657776024e5b5dbd8ec12c9d40bd39851c1488f1
TakuyaOSADA <osada@TakuyaOSADA-no-Mac-mini.local>
2014-05-06 11:08:18 +0900

    create package install cookbook

---
+++ site-cookbooks/install-packages/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: install-packages
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+%w{gcc make zlib-devel perl-ExtUtils-MakeMaker httpd httpd-devel openssl-devel libyaml-devel libxml2-devel libxslt-devel libffi-devel readline-devel pcre-devel sqlite-devel mysql mysql-server mysql-devel curl-devel nkf}.each do |pkg|
+	package pkg do
+		options "-y"
+		action :install
+	end
+end
