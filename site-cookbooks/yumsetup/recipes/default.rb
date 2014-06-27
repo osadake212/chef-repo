61ec239fc74644448cee322e41444e71418d248d
Osada Takuya <osadake.212.dev@gmail.com>
2014-06-27 20:12:37 +0900

    modify setup yum-epel repo recipe

--- site-cookbooks/yumsetup/recipes/default.rb
+++ site-cookbooks/yumsetup/recipes/default.rb
@@ -7,17 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
+FILE_NAME_EPEL_RPM = 'epel-release-6-8.noarch.rpm'
+PATH_USER_LOCAL = '/usr/local/src'
+PATH_EPEL_RPM = "#{PATH_USER_LOCAL}/#{FILE_NAME_EPEL_RPM}"
+
 # copy epel rpm file
 cookbook_file "yum-epel.rpm" do
-	source "epel-release-6-8.noarch.rpm"
-	path "/usr/local/src/epel-release-6-8.noarch.rpm"
-	not_if { File.exists?('usr/local/src/epel-release-6-8.noarch.rpm') }
+	source FILE_NAME_EPEL_RPM
+	path PATH_EPEL_RPM
+	not_if { File.exists?(PATH_EPEL_RPM) }
 end
 
 # add epel repo
 execute "yum-epel-rpm" do
 	user "root"
-	command "rpm -ivh /usr/local/src/epel-release-6-8.noarch.rpm"
+	command "rpm -ivh #{PATH_EPEL_RPM}"
 	action :run
 	not_if { File.exists?('/etc/yum.repos.d/epel.repo') }
 end
